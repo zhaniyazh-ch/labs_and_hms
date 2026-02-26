6407f4c6ae19e2a7ccca4fd90db233dcc27ac6e8
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 09:11:40 +0500

    4 esep

--- lab3/lambda.py
+++ lab3/lambda.py
@@ -13,3 +13,8 @@ print(sorted_words)
 numbers = [5, 12, 7, 20, 33, 8]
 result = list(filter(lambda x: x % 2 == 0 and x > 10, numbers))
 print(result)
+
+#4 Преобразование списка с условиями
+numbers = [1, 2, 3, 4, 5, 6]
+result = list(map(lambda x: x**2 if x % 2 == 0 else x*3, numbers))
+print(result)
