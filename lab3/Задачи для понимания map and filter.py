95e36a4acddf18ee5900c07e4146020e0d7cb5d9
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 20:44:47 +0500

    3 esep

--- lab3/Задачи для понимания map and filter.py
+++ lab3/Задачи для понимания map and filter.py
@@ -7,3 +7,8 @@ print(doubled)
 words = ["кот", "машина", "арбуз", "дом"]
 result = list(map(lambda w: w.upper() + "!" if len(w) > 3 else w.upper(), words))
 print(result)
+
+#3 Оставить только чётные числа (filter)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+print(evens)
