788876034b0c92ba295eeb607eb130b1aedd9b4b
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 20:58:23 +0500

    2 esep

--- lab3/генераторы.py
+++ lab3/генераторы.py
@@ -7,3 +7,15 @@ def even_numbers(n):
             yield i
 for x in even_numbers(10):
     print(x)
+
+#2 Фильтрация строк
+def filter_words(words):
+    for w in words:
+        if len(w) > 4:
+            if "а" in w:
+                yield "с а"
+            else:
+                yield w
+words = ["кот", "машина", "арбуз", "дом"]
+for w in filter_words(words):
+    print(w)
