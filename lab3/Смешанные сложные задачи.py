dc55da9c1d1087abda6d5939d14dcdee79571be6
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 20:41:06 +0500

    1 esep

---
+++ lab3/Смешанные сложные задачи.py
@@ -0,0 +1,5 @@
+#1 Генератор и фильтр по сложному условию
+#2 List comprehension + lambda + условия
+#3 Генератор + map + filter + условия
+#4 Dict comprehension + lambda + вложенные условия
+#5 Смешанный comprehension + генератор
