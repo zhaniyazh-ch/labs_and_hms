8db9b5c043560b3b2a0365e9144d3a9077569818
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 20:48:44 +0500

    1 esep

--- lab3/Смешанные сложные задачи.py
+++ lab3/Смешанные сложные задачи.py
@@ -1,5 +1,22 @@
 #1 Генератор и фильтр по сложному условию
-#2 List comprehension + lambda + условия
-#3 Генератор + map + filter + условия
-#4 Dict comprehension + lambda + вложенные условия
-#5 Смешанный comprehension + генератор
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+def special_numbers(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            yield "FizzBuzz"
+        elif i % 3 == 0:
+            yield "Fizz"
+        elif i % 5 == 0:
+            yield "Buzz"
+        elif is_prime(i):
+            yield "простое"
+        else:
+            yield i
+for x in special_numbers(15):
+    print(x)
