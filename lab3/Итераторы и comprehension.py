06a598847d41dac75f40414112774d3913f454a7
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 09:32:50 +0500

    3 esep

--- lab3/Итераторы и comprehension.py
+++ lab3/Итераторы и comprehension.py
@@ -7,3 +7,8 @@ matrix = [[1,2,3], [4,5,6], [7,8,9]]
 result = [(lambda row: row[0] * row[1] * row[2])(r) for r in matrix]
 print(result)
 
+#3 Фильтр строк
+words = ["кот", "машина", "ананас", "дом"]
+filtered = [w for w in words if len(w) > 4 and "а" not in w]
+print(filtered)
+
