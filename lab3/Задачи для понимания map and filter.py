14751d673be15ae0651a574b2fc0d37aaeac1cde
zhanek-zh <zh.zanabai@gmail.com>
2026-02-26 20:43:19 +0500

    2 esep

--- lab3/Задачи для понимания map and filter.py
+++ lab3/Задачи для понимания map and filter.py
@@ -2,3 +2,8 @@
 numbers = [1, 2, 3, 4, 5]
 doubled = list(map(lambda x: x * 2, numbers))
 print(doubled)
+
+#2 Преобразовать строки в заглавные (map)
+words = ["кот", "машина", "арбуз", "дом"]
+result = list(map(lambda w: w.upper() + "!" if len(w) > 3 else w.upper(), words))
+print(result)
