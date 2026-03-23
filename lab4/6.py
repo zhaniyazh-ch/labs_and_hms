723959ba104899bee3b782e90ba01de94a0986dc
zhanek-zh <zh.zanabai@gmail.com>
2026-03-23 12:47:01 +0500

    Класс Event

---
+++ lab4/6.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+class Event:
+    def __init__(self, type: str, data: dict):
+        self.type = type
+        self.data = data
+        self.timestamp = datetime.now()
+    def __repr__(self):
+        return f"Event(type='{self.type}', data={self.data}, timestamp='{self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}')"
+e = Event("ATTACK", {"damage": 20})
+print(e)
