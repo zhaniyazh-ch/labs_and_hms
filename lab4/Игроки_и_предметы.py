96f23433df0b33282e5377f75e656558a136d222
zhanek-zh <zh.zanabai@gmail.com>
2026-03-26 09:05:00 +0500

    Класс Item

--- lab4/Игроки_и_предметы.py
+++ lab4/Игроки_и_предметы.py
@@ -32,3 +32,18 @@ class Player:
         return cls(_id, name, hp)
 p = Player.from_string("2, alice , 90")
 print(p)#2
+class Item:
+    def __init__(self, id, name, power):
+        self.id = id
+        self.name = name.strip().title()
+        self.power = power
+    def __str__(self):
+        return f"Item(id={self.id}, name='{self.name}', power={self.power})"
+    def __eq__(self, other):
+        if not isinstance(other, Item):
+            return False
+        return self.id == other.id
+    def __hash__(self):
+        return hash(self.id)
+i = Item(1, " Sword ", 50)
+print(i)#3
